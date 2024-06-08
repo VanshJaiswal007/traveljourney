c9a4a3bb0368ea06098bb8b6383902abc737fdb9
VanshJaiswal007 <mrinvis123@gmail.com>
2024-06-09 05:15:55 +0530

    bug fixed

--- src/data.jsx
+++ src/data.jsx
@@ -25,14 +25,5 @@ export default [
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "https://imgs.search.brave.com/ShtNxxFxVw9_yxaJ_sEuiOcPdm_GttNTDN_dGiozW34/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/c2FuaXR5LmlvL2lt/YWdlcy80YWFuczBp/bi9wcm9kdWN0aW9u/LzY0NTU0MjdkMjcx/OTM0NzExMTM4OTU1/MDg2ZWRmZmU2NTg3/ZTg1NTItMTQ0MHgx/NDQwLmpwZz93PTM4/NDAmaD0zODQwJmZt/PXdlYnAmcT0xMDAm/Zml0PW1heCZhdXRv/PWZvcm1hdA"
-    },
-    {
-        title: "Geirangerfjord",
-        location: "NORWAY",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "01 Oct, 2021",
-        endDate: "18 Nov, 2021",
-        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://imgs.search.brave.com/ShtNxxFxVw9_yxaJ_sEuiOcPdm_GttNTDN_dGiozW34/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/c2FuaXR5LmlvL2lt/YWdlcy80YWFuczBp/bi9wcm9kdWN0aW9u/LzY0NTU0MjdkMjcx/OTM0NzExMTM4OTU1/MDg2ZWRmZmU2NTg3/ZTg1NTItMTQ0MHgx/NDQwLmpwZz93PTM4/NDAmaD0zODQwJmZt/PXdlYnAmcT0xMDAm/Zml0PW1heCZhdXRv/PWZvcm1hdA"
     }
 ]
